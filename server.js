073b59f4d307b7cec54ed5898465255a3fbc15f1
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-27 04:02:57 +0100

    first commit

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+const exp = require("constants");
 const express = require("express");
 const app = express();
 const path = require("path");
 // const { engine } = require("express-handlebars");
 
 // CRUD METHODS:
+app.use(express.static("public"));
 // get
 
 // call back function / end point
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("public" , "home.html"));
+  res.sendFile(path.resolve("public", "home.html"));
 });
 app.get("/ourservicers", (req, res) => {
-  res.sendFile(path.resolve("public" , "contact.html"));
+  res.sendFile(path.resolve("public", "services.html"));
 });
 app.get("/contactus", (req, res) => {
-  res.sendFile(path.resolve("public" , "services.html"));
+  res.sendFile(path.resolve("public", "contact.html"));
 });
 
 // post
