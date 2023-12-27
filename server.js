e536bc72aae23bb8190a5d804fa3be5175625049
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-27 05:21:14 +0100

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
-const exp = require("constants");
+// const exp = require("constants");
 const express = require("express");
 const app = express();
-const path = require("path");
+
+// const path = require("path");
 const { engine } = require("express-handlebars");
 
 //
-
+const workingHoursMiddleware = (req, res, next) => {
+  const now = new Date();
+  const dayofWeek = now.getDay();
+  const hourodFay = now.getHours();
+  if (dayofWeek >= 1 && dayofWeek <= 5 && hourodFay >= 9 && hourodFay < 17) {
+    next();
+  } else {
+    res.send("Sorry, the web APP is only available Monday to Friday  to 17  ");
+  }
+};
 app.use(express.static("public"));
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
